5b91df8ddcf1d2f65f9c2527f9caef78e25bd1c1
unknown <Shoaib soomro>
2022-09-14 00:48:41 -0700

    first commit

---
+++ Server.mjs
@@ -0,0 +1,103 @@
+import express from "express";
+import cors from "cors";
+import { nanoid } from "nanoid";
+
+const App = express();
+
+App.use(express.json());
+App.use(cors());
+
+const Port = process.env.PORT || 3000;
+
+const UserBae = []
+
+function randomNumber(){
+    return Math.floor(Math.random() * 10000)
+}
+
+App.post('/signup',(res , req)=>{
+  
+    let body = req.body ;
+
+    if(!body.firstName 
+        || !body.LastName 
+        ||  !body.email 
+        ||    !body.password
+        ){
+             req.status(400).send(`
+             require missing field please requst example :
+             {
+                firstName : "jon" ,
+                LastName :  "Sing",
+                email :  "jonsin@gmail.com",
+                password : "12345"
+            
+                
+             }
+             `)
+             return ;
+        }
+        let NewUser  = {
+            userId : nanoid(),
+            firstName : req.firstName,
+            LastName : req.LastName,
+            email : req.email,
+            password : req.password
+        }    
+        UserBae.push(NewUser )
+        res.status(201).send("user is created ")
+})
+
+App.post('/Login', (res, req)=>{
+    
+    if(
+           !body.email 
+        ||  !body.password
+        ){
+             req.status(400).send(`
+             require missing field please requst example :
+             {
+                
+                email :  "jonsin@gmail.com",
+                password : "12345"
+            
+                
+             }
+             `)
+             return ;
+        }
+
+        let isFound = false
+
+        for(let i = 0; i<UserBae.length; i++){
+            if(UserBae[i].email === body.email){
+                 isFound = true
+                if(UserBae[i].password === body.password){    // correct password 
+                    res.status(200).send({
+                        firstName: UserBae[i].firstName,
+                        LastName : UserBae[i].LastName,
+                        email : UserBae[i].email,
+                        message : "Login is successful"
+                    }) 
+                    return
+                }else{       // password incorrect
+                    res.status(401).send({
+                        message : 'incorrect password'
+                    })
+                    return 
+                }
+            }
+            
+        }
+        if(!isFound){
+            res.status(404).send({
+                message : "user is not found"
+            })
+
+        }
+})
+
+
+App.listen(Port,()=>{
+    console.log(`your server is working ${Port}`)
+})
